62b20a8de1dfd04ae03b1c08e25302106cd61bb0
Pham Long <vanlong200880@gmail.com>
2017-02-05 22:35:31 +0700

    Update source

--- public/frontend/js/common.js
+++ public/frontend/js/common.js
@@ -1,6 +1,6 @@
 (function ($) { 
   //window scroll menu
   $(window).bind("scroll", function() {
-    $(window).scrollTop() > 50 ? $("#header").addClass("fixed") : $("#header").removeClass("fixed");
+    $(window).scrollTop() > 500 ? $("#header").addClass("fixed") : $("#header").removeClass("fixed");
   });
 })(jQuery);
