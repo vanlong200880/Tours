fdcce11ad060414d2bf1764bbdd5e5ef25156720
Pham Long <vanlong200880@gmail.com>
2017-03-19 23:23:19 +0700

    Update source

--- public/frontend/js/common.js
+++ public/frontend/js/common.js
@@ -1,5 +1,8 @@
 (function ($) { 
   //window scroll menu
+  $(".current-position").on('click', '.btn-filter-advance', function(){
+    $(".filter-advance").toggle('fast');
+  });
   $(window).bind("scroll", function() {
     $(window).scrollTop() > 500 ? $("#header").addClass("fixed") : $("#header").removeClass("fixed");
   });
